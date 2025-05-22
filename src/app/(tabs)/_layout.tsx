6b258dda725b51bad31cc026071db0277946de4b
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-05-23 01:45:32 +0800

    setup screens

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,11 +1,61 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import colors from "@/constants/colors";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="people" />
-      <Tabs.Screen name="favorites" />
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        headerTintColor: colors.tint,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        headerStyle: {
+          backgroundColor: colors.black,
+        },
+        sceneStyle: {
+          backgroundColor: colors.darkGrey,
+        },
+        tabBarActiveTintColor: colors.tint,
+        tabBarStyle: {
+          backgroundColor: colors.black,
+          borderTopWidth: 1,
+          borderTopColor: colors.tint,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerTitle: "Star Wars",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="people"
+        options={{
+          title: "All Characters",
+          tabBarLabel: "People",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: "My Favorites",
+          tabBarLabel: "Favorites",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="star-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export default {
+  black: "#000000",
+  white: "#FFFFFF",
+  darkGrey: "#1D1E20",
+  tint: "#FADE4b",
+};
